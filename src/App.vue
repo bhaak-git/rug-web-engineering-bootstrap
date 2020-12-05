26b0ad790ec1aa5323c10dcb8aef79d8538a007d
Max Vos <max.vos7@gmail.com>
2020-12-05 10:16:15 +0100

    first draft

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <h1>This website can retrieve the public repos of any user in github:</h1>
+        <input type="text" v-model="userName">
+        <button @click="fetchData">Search</button>
+        <div v-if="repos.length != 0">
+            <h2>Results</h2>
+            <div>
+                <ul>
+                    <li v-for="repo in repos" :key="repo.id">
+                        {{ repo.name }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    data() {
+      return {
+        userName: null,
+        repos: []
+      }
+    },
+    methods: {
+      async fetchData() {
+        let {data} = await this.$http.get(`https://api.github.com/users/${this.userName}/repos`);
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+        this.repos = data;
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from "axios";
 
-Vue.config.productionTip = false
+Vue.prototype.$http = Axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
