fb2961967eb1aea9747eb599e10636077854b71c
Max Vos <max.vos7@gmail.com>
2020-12-08 13:12:34 +0100

    feat: Using a different api. This allows us to post something.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
     <div>
-        <h1>This website can retrieve the public repos of any user in github:</h1>
-        <input type="text" v-model="userName">
-        <button @click="fetchData">Search</button>
-        <div v-if="repos.length != 0">
-            <h2>Results</h2>
-            <div>
-                <ul>
-                    <li v-for="repo in repos" :key="repo.id">
-                        {{ repo.name }}
-                    </li>
-                </ul>
+        <h1>Lets keep track of users</h1>
+        <div>
+            <h3>Here is a list of all the users</h3>
+            <div v-if="users.length > 0">
+                <div v-for="user in users" :key="user.id">
+                    <button @click="highlightUser(user.id)">
+                        {{ user.first_name }} {{ user.last_name }}
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h3>Create a new user</h3>
+            <form @submit.prevent="submitUser" v-if="!loading">
+                <div>
+                    <label for="first_name">
+                        First name
+                    </label>
+                    <input type="text" id="first_name" v-model="form.first_name">
+                </div>
+                <div>
+                    <label for="last_name">
+                        Last name
+                    </label>
+                    <input type="text" id="last_name" v-model="form.last_name">
+                </div>
+                <div>
+                    <label for="email">
+                        Email
+                    </label>
+                    <input type="text" id="email" v-model="form.email">
+                </div>
+                <button type="submit">Create user</button>
+            </form>
+            <p v-else>Loading...</p>
+        </div>
+        <div v-if="highlightedUser != null">
+            <h3>Highlighted user</h3>
+            <div v-for="(field, key) in highlightedUser" :key="field">
+                <span v-if="key !== 'avatar'">
+                    <strong>{{ key }}:</strong> {{ field }}
+                </span>
+                <span v-else>
+                    <img :src="field">
+                </span>
             </div>
         </div>
     </div>
@@ -18,30 +52,37 @@
 
 <script>
   export default {
-    name: 'App',
     data() {
       return {
-        userName: null,
-        repos: []
+        users: [],
+        form: {
+          first_name: null,
+          last_name: null,
+          email: null,
+        },
+        loading: false,
+        highlightedUser: null
       }
     },
+    mounted() {
+      this.fetchUsers();
+    },
     methods: {
-      async fetchData() {
-        let {data} = await this.$http.get(`https://api.github.com/users/${this.userName}/repos`);
+      async fetchUsers() {
+        let {data} = await this.$http.get('users?per_page=100');
 
-        this.repos = data;
+        this.users = data.data;
+      },
+      async submitUser() {
+        this.loading = true;
+        let {data} = await this.$http.post('users?delay=3', this.form);
+        this.loading = false;
+        this.users.push(data);
+      },
+      async highlightUser(userId) {
+        let {data} = await this.$http.get('users/' + userId);
+        this.highlightedUser = data.data;
       }
     }
   }
 </script>
-
-<style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import Axios from "axios";
 
+Axios.defaults.baseURL = "https://reqres.in/api/";
+
 Vue.prototype.$http = Axios;
 
 Vue.config.productionTip = false;
