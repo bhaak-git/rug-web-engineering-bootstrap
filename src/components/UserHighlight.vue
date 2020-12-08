9156b2eb87022a8850d5494a4d419a4f5ad52122
Max Vos <max.vos7@gmail.com>
2020-12-08 17:20:52 +0100

    feat: possible refactor if time allows

---
+++ src/components/UserHighlight.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>Highlighted user</h3>
+        <span v-if="user == null && !loading">No highlighted user</span>
+        <div v-else-if="!loading">
+            <div v-for="(field, key) in user" :key="field">
+                <span v-if="key !== 'avatar'">
+                    <strong>{{ key }}:</strong> {{ field }}
+                </span>
+                <span v-else>
+                    <img :src="field">
+                </span>
+            </div>
+        </div>
+        <p v-else>Loading...</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {
+        required: true
+      },
+      loading: {
+        required: true,
+        type: Boolean
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
