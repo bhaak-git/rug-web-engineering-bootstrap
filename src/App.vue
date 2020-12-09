2086c94224ff634e04e408d48f38e5fafa27f2ca
bhaak-git <17254355+bhaak-git@users.noreply.github.com>
2020-12-09 14:59:22 +0100

    Format App.vue with 'prettier', disable bootstrap

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@
                         <td>{{ user.first_name }}</td>
                         <td>{{ user.last_name }}</td>
                         <td>
-                            <button @click="highlightUser(user.id)">click here</button>
+                            <button @click="highlightUser(user.id)">
+                                click here
+                            </button>
                         </td>
                     </tr>
                 </table>
@@ -19,22 +21,24 @@
             <h3>Create a new user</h3>
             <form @submit.prevent="submitUser" v-if="!loading.form">
                 <div>
-                    <label for="first_name">
-                        First name
-                    </label>
-                    <input type="text" id="first_name" v-model="form.first_name">
+                    <label for="first_name"> First name </label>
+                    <input
+                        type="text"
+                        id="first_name"
+                        v-model="form.first_name"
+                    />
                 </div>
                 <div>
-                    <label for="last_name">
-                        Last name
-                    </label>
-                    <input type="text" id="last_name" v-model="form.last_name">
+                    <label for="last_name"> Last name </label>
+                    <input
+                        type="text"
+                        id="last_name"
+                        v-model="form.last_name"
+                    />
                 </div>
                 <div>
-                    <label for="email">
-                        Email
-                    </label>
-                    <input type="text" id="email" v-model="form.email">
+                    <label for="email"> Email </label>
+                    <input type="text" id="email" v-model="form.email" />
                 </div>
                 <button type="submit">Create user</button>
             </form>
@@ -43,16 +47,18 @@
         <div>
             <h3>Highlighted user</h3>
 
-            <span v-if="highlightedUser == null && !loading.highlight">No highlighted user</span>
+            <span v-if="highlightedUser == null && !loading.highlight"
+                >No highlighted user</span
+            >
 
             <div v-else-if="!loading.highlight">
                 <div v-for="(field, key) in highlightedUser" :key="field">
-                <span v-if="key !== 'avatar'">
-                    <strong>{{ key }}:</strong> {{ field }}
-                </span>
-                <span v-else>
-                    <img :src="field">
-                </span>
+                    <span v-if="key !== 'avatar'">
+                        <strong>{{ key }}:</strong> {{ field }}
+                    </span>
+                    <span v-else>
+                        <img :src="field" />
+                    </span>
                 </div>
             </div>
             <p v-else>Loading...</p>
@@ -61,55 +67,57 @@
 </template>
 
 <script>
-  export default {
+export default {
     data() {
-      return {
-        users: [],
-        form: null,
-        loading: {
-          list: false,
-          form: false,
-          highlight: false
-        },
-        highlightedUser: null
-      }
-    },
-    mounted() {
-      this.resetForm();
-      this.fetchUsers();
-    },
-    methods: {
-      async fetchUsers() {
-        this.loading.list = true;
-        let {data} = await this.$http.get('users?per_page=100&delay=1');
-
-        this.users = data.data;
-        this.loading.list = false;
-      },
-      async submitUser() {
-        this.loading.form = true;
-        let {data} = await this.$http.post('users?delay=1', this.form);
-        this.resetForm();
-        this.users.push(data);
-        this.loading.form = false;
-      },
-      async highlightUser(userId) {
-        this.loading.highlight = true;
-        let {data} = await this.$http.get('users/' + userId + '?delay=1');
-        this.highlightedUser = data.data;
-        this.loading.highlight = false;
-      },
-      resetForm() {
-        this.form = {
-          first_name: null,
-          last_name: null,
-          email: null,
+        return {
+            users: [],
+            form: null,
+            loading: {
+                list: false,
+                form: false,
+                highlight: false,
+            },
+            highlightedUser: null,
         };
-      }
-    }
-  }
+    },
+
+    mounted() {
+        this.resetForm();
+        this.fetchUsers();
+    },
+
+    methods: {
+        async fetchUsers() {
+            this.loading.list = true;
+            let { data } = await this.$http.get("users?per_page=100&delay=1");
+
+            this.users = data.data;
+            this.loading.list = false;
+        },
+        async submitUser() {
+            this.loading.form = true;
+            let { data } = await this.$http.post("users?delay=1", this.form);
+            this.resetForm();
+            this.users.push(data);
+            this.loading.form = false;
+        },
+        async highlightUser(userId) {
+            this.loading.highlight = true;
+            let { data } = await this.$http.get("users/" + userId + "?delay=1");
+            this.highlightedUser = data.data;
+            this.loading.highlight = false;
+        },
+        resetForm() {
+            this.form = {
+                first_name: null,
+                last_name: null,
+                email: null,
+            };
+        },
+    },
+};
 </script>
 
-<style>
-    @import '~bootstrap/dist/css/bootstrap.css';
-</style>
+<!--<style>
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>-->
--- src/main.js
+++ src/main.js
@@ -10,5 +10,5 @@ Vue.prototype.$http = Axios;
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app');
